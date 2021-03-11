f1e69f6e83e4015d45d73fb06458f1ff3571f5da
Ernesto Alejo <ernestokarim@gmail.com>
2021-03-11 14:18:13 +0100

    infra: Corrige un typo al configurar la ruta de los binarios.
    Change-Id: Ib679e585c10225fd03429831d40b7fe731971000

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -9,7 +9,7 @@ GOOGLE_PROJECT=altipla-tools
 configure-google-cloud
 
 mkdir -p tmp/bin
-run "GOBIN=tmp/bin go install ./cmd/..."
+run "GOBIN=$(pwd)/tmp/bin go install ./cmd/..."
 run "gsutil -h 'Cache-Control: no-cache' cp tmp/bin/* gs://tools.altipla.consulting/tools/"
 
 run "gsutil -h 'Cache-Control: no-cache' cp install.sh gs://tools.altipla.consulting/install/tools"
