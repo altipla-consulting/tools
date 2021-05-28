3a4b12424664aae5560e74970e8c8fcb05e629ac
Ernesto Alejo <ernestokarim@gmail.com>
2021-05-28 10:30:30 +0200

    pub: Configura el logging de depuración con flag.
    Change-Id: Ic67e97d0b92c4d03dacbacd63a75cd31a7f6739b

--- cmd/pub/main.go
+++ cmd/pub/main.go
@@ -4,12 +4,23 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
+	flag "github.com/spf13/pflag"
 	"libs.altipla.consulting/errors"
 
 	"tools.altipla.consulting/cmd/pub/internal/publish"
 )
 
 func main() {
+	var flagDebug bool
+	flag.BoolVarP(&flagDebug, "debug", "d", false, "Debug logging")
+	flag.Parse()
+
+	log.SetFormatter(new(log.TextFormatter))
+	if flagDebug {
+		log.SetLevel(log.DebugLevel)
+		log.Debug("DEBUG log level activated")
+	}
+
 	if err := publish.Run(); err != nil {
 		log.Error(err.Error())
 		log.Debug(errors.Stack(err))
