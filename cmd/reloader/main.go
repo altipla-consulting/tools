1906ecc62bd9070b9dac6c5c9f05e064608f10c7
Ernesto Alejo <ernestokarim@gmail.com>
2021-08-30 17:36:27 +0200

    reloader: Evita emitir el stacktrace en los errores directamente.
    Change-Id: Ia9f5e982f76aa840a13fb544284d4611d10be79e

--- cmd/reloader/main.go
+++ cmd/reloader/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"os"
+
 	log "github.com/sirupsen/logrus"
 	"libs.altipla.consulting/errors"
 )
 
 func main() {
 	if err := CmdRoot.Execute(); err != nil {
-		log.Fatal(errors.Stack(err))
+		log.Debug(errors.Stack(err))
+		os.Exit(1)
 	}
 }
