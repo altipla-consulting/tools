2c78bfcc7c5684f436185fb375c3e3a5bdd0a76b
Ernesto Alejo <ernestokarim@gmail.com>
2021-12-07 19:17:08 +0100

    configure-dev-machine: Install tools using our DEB packages repository.

--- cmd/configure-dev-machine/internal/installers/ins_tools.go
+++ cmd/configure-dev-machine/internal/installers/ins_tools.go
@@ -18,8 +18,10 @@ func (ins *insTools) Check() (*CheckResult, error) {
 
 func (ins *insTools) Install() error {
 	script := `
-  	rm -f ~/bin/ci ~/bin/configure-dev-machine ~/bin/gaestage ~/bin/gendc ~/bin/impsort ~/bin/jnet ~/bin/linter ~/bin/previewer-netlify ~/bin/pub ~/bin/releaser ~/bin/reloader ~/bin/wave
-		curl -sL https://tools.altipla.consulting/install/all | sudo bash
+		curl https://europe-west1-apt.pkg.dev/doc/repo-signing-key.gpg | sudo apt-key add -
+		echo 'deb https://europe-west1-apt.pkg.dev/projects/altipla-tools acpublic main' | sudo tee /etc/apt/sources.list.d/acpublic.list
+		sudo apt update
+		sudo apt install tools/acpublic
   `
 	return errors.Trace(run.Shell(script))
 }
