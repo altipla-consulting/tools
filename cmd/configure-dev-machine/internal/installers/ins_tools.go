5e67c690087ce34bf7a605965d0644bfa1882f08
Ernesto Alejo <ernestokarim@gmail.com>
2021-12-07 20:46:04 +0100

    configure-dev-machine: Accept installation of additional tools automatically.

--- cmd/configure-dev-machine/internal/installers/ins_tools.go
+++ cmd/configure-dev-machine/internal/installers/ins_tools.go
@@ -21,7 +21,7 @@ func (ins *insTools) Install() error {
 		curl https://europe-west1-apt.pkg.dev/doc/repo-signing-key.gpg | sudo apt-key add -
 		echo 'deb https://europe-west1-apt.pkg.dev/projects/altipla-tools acpublic main' | sudo tee /etc/apt/sources.list.d/acpublic.list
 		sudo apt update
-		sudo apt install tools/acpublic
+		sudo apt install -y tools/acpublic
   `
 	return errors.Trace(run.Shell(script))
 }
