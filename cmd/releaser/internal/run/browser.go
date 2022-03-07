2305cd436c95c915be64e84886b4dc6d3c89bb61
Ernesto Alejo <ernestokarim@gmail.com>
2022-03-07 09:23:43 +0000

    releaser: Print the URL instead of opening the browser if the utility is not installed.

--- cmd/releaser/internal/run/browser.go
+++ cmd/releaser/internal/run/browser.go
@@ -3,9 +3,19 @@ package run
 import (
 	"os/exec"
 
+	log "github.com/sirupsen/logrus"
 	"libs.altipla.consulting/errors"
 )
 
 func OpenBrowser(url string) error {
+	if _, err := exec.LookPath("xdg-open"); err != nil {
+		if errors.Is(err, exec.ErrNotFound) {
+			log.Println("Open the following URL in your browser:")
+			log.Println("\t" + url)
+			return nil
+		}
+
+		return errors.Trace(err)
+	}
 	return errors.Trace(exec.Command("xdg-open", url).Start())
 }
