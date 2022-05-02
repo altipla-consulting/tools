51148f8732283e2f33d2d13c2e0ec47799495d2e
Ernesto Alejo <ernestokarim@gmail.com>
2022-05-02 12:45:09 +0200

    releaser: Depura cuando abrimos una URL en el navegador.
    En WSL algunas URLs no se abren correctamente y es necesario hacerlo a mano.

--- cmd/releaser/internal/run/browser.go
+++ cmd/releaser/internal/run/browser.go
@@ -4,12 +4,14 @@ import (
 	"os"
 	"os/exec"
 
+	log "github.com/sirupsen/logrus"
 	"libs.altipla.consulting/errors"
 )
 
 var ErrCannotOpenBrowser = errors.New("run: cannot open browser")
 
 func OpenBrowser(url string) error {
+	log.Debug("Open browser with URL: ", url)
 	if _, err := exec.LookPath("xdg-open"); err != nil {
 		var ee *exec.Error
 		if errors.As(err, &ee) && errors.Is(ee.Err, exec.ErrNotFound) {
